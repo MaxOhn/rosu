b47000f51674ffd3aad4960cee375e135b650848
MaxOhn <ohn.m@hotmail.de>
2020-07-31 21:24:05 +0200

    Minor error msg improvement and using tokio::test

--- src/backend/error.rs
+++ src/backend/error.rs
@@ -32,14 +32,14 @@ impl From<FromUtf8Error> for OsuError {
 impl fmt::Display for OsuError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Self::ReqBuilder(e) => write!(f, "{}", e),
-            Self::Json(e) => write!(f, "{}", e),
-            Self::FromUtf8(e) => write!(f, "{}", e),
-            Self::ParseError(e) => write!(f, "{}", e),
-            Self::BadResponse(e) => write!(f, "{}", e),
-            Self::Other(e) => write!(f, "{}", e),
-            Self::FetchError(e) => write!(f, "Error while fetching: {}", e),
-            Self::InvalidUrl(url) => write!(f, "Could not parse \"{}\" into url", url),
+            Self::ReqBuilder(e) => f.write_str(e),
+            Self::Json(e) => write!(f, "json error: {}", e),
+            Self::FromUtf8(e) => write!(f, "utf8 error: {}", e),
+            Self::ParseError(e) => f.write_str(e),
+            Self::BadResponse(e) => write!(f, "bad response: {}", e),
+            Self::Other(e) => f.write_str(e),
+            Self::FetchError(e) => write!(f, "error while fetching: {}", e),
+            Self::InvalidUrl(url) => write!(f, "could not parse \"{}\" into url", url),
             Self::NoResults(e) => write!(f, "API response contained no {} elements", e),
         }
     }
--- tests/requests.rs
+++ tests/requests.rs
@@ -3,13 +3,11 @@ extern crate rosu;
 use chrono::DateTime;
 use rosu::{
     backend::{
-        requests::{BeatmapRequest, BestRequest, MatchRequest, ScoreRequest, UserRequest},
-        Osu,
+        BeatmapRequest, BestRequest, MatchRequest, Osu, RecentRequest, ScoreRequest, UserRequest,
     },
     models::*,
 };
 use std::env;
-use tokio::runtime::Runtime;
 
 fn init() -> String {
     let _ = env_logger::builder().is_test(true).try_init();
@@ -17,95 +15,92 @@ fn init() -> String {
     env::var("OSU_TOKEN").expect("Could not find env variable 'OSU_TOKEN'")
 }
 
-#[test]
-fn get_user() {
-    let mut rt = Runtime::new().unwrap();
-    rt.block_on(async move {
-        let osu_key = init();
-        let osu = Osu::new(osu_key);
-        let user = UserRequest::with_username("Badewanne3")
-            .queue_single(&osu)
-            .await
-            .unwrap()
-            .unwrap();
-        let join_date = DateTime::parse_from_rfc3339("2012-12-24T19:48:09-00:00").unwrap();
-        assert_eq!(user.join_date, join_date);
-        let best = user.get_top_scores(&osu, 8, GameMode::STD).await.unwrap();
-        assert_eq!(best.len(), 8);
-    });
+#[tokio::test]
+async fn get_user() {
+    let osu_key = init();
+    let osu = Osu::new(osu_key);
+    let user = UserRequest::with_username("Badewanne3")
+        .queue_single(&osu)
+        .await
+        .unwrap()
+        .unwrap();
+    let join_date = DateTime::parse_from_rfc3339("2012-12-24T19:48:09-00:00").unwrap();
+    assert_eq!(user.join_date, join_date);
+    let best = user.get_top_scores(&osu, 8, GameMode::STD).await.unwrap();
+    assert_eq!(best.len(), 8);
 }
 
-#[test]
-fn get_maps() {
-    let mut rt = Runtime::new().unwrap();
-    rt.block_on(async move {
-        let osu_key = init();
-        let osu = Osu::new(osu_key);
-        let maps = BeatmapRequest::new()
-            .mapset_id(1086483)
-            .queue(&osu)
-            .await
-            .unwrap();
-        assert_eq!(maps.len(), 2);
-        let map = maps.get(0).unwrap();
-        assert_eq!(map.creator, "Mao");
-        let leaderboard = map.get_global_leaderboard(&osu, 7).await.unwrap();
-        assert_eq!(leaderboard.len(), 7);
-    });
+#[tokio::test]
+async fn get_maps() {
+    let osu_key = init();
+    let osu = Osu::new(osu_key);
+    let maps = BeatmapRequest::new()
+        .mapset_id(1086483)
+        .queue(&osu)
+        .await
+        .unwrap();
+    assert_eq!(maps.len(), 2);
+    let map = maps.get(0).unwrap();
+    assert_eq!(map.creator, "Mao");
+    let leaderboard = map.get_global_leaderboard(&osu, 7).await.unwrap();
+    assert_eq!(leaderboard.len(), 7);
 }
 
-#[test]
-fn get_score() {
-    let mut rt = Runtime::new().unwrap();
-    rt.block_on(async move {
-        let osu_key = init();
-        let osu = Osu::new(osu_key);
-        let scores = ScoreRequest::with_map_id(905576)
-            .username("spamblock")
-            .mode(GameMode::MNA)
-            .queue(&osu)
-            .await
-            .unwrap();
-        assert_eq!(scores.len(), 4);
-        let score = scores.get(2).unwrap();
-        assert_eq!(score.max_combo, 1293);
-        let user = score.get_user(&osu, GameMode::MNA).await.unwrap();
-        assert_eq!(user.username, "spamblock");
-    })
+#[tokio::test]
+async fn get_score() {
+    let osu_key = init();
+    let osu = Osu::new(osu_key);
+    let scores = ScoreRequest::with_map_id(905576)
+        .username("spamblock")
+        .mode(GameMode::MNA)
+        .queue(&osu)
+        .await
+        .unwrap();
+    assert_eq!(scores.len(), 4);
+    let score = scores.get(2).unwrap();
+    assert_eq!(score.max_combo, 1293);
+    let user = score.get_user(&osu, GameMode::MNA).await.unwrap();
+    assert_eq!(user.username, "spamblock");
 }
 
-#[test]
-fn get_best() {
-    let mut rt = Runtime::new().unwrap();
-    rt.block_on(async move {
-        let osu_key = init();
-        let osu = Osu::new(osu_key);
-        let scores = BestRequest::with_username("Badewanne3")
-            .mode(GameMode::TKO)
-            .limit(8)
-            .queue(&osu)
-            .await
-            .unwrap();
-        assert_eq!(scores.len(), 8);
-        let score = scores.get(6).unwrap();
-        assert_eq!(score.count100, 22);
-    })
+#[tokio::test]
+async fn get_best() {
+    let osu_key = init();
+    let osu = Osu::new(osu_key);
+    let scores = BestRequest::with_username("Badewanne3")
+        .mode(GameMode::TKO)
+        .limit(8)
+        .queue(&osu)
+        .await
+        .unwrap();
+    assert_eq!(scores.len(), 8);
+    let score = scores.get(6).unwrap();
+    assert_eq!(score.count100, 22);
 }
 
-#[test]
-fn get_match() {
-    let mut rt = Runtime::new().unwrap();
-    rt.block_on(async move {
-        let osu_key = init();
-        let osu = Osu::new(osu_key);
-        let osu_match = MatchRequest::with_match_id(58494587)
-            .queue_single(&osu)
-            .await
-            .unwrap();
-        assert_eq!(osu_match.match_id, 58494587);
-        assert_eq!(osu_match.games.len(), 8);
-        for game in osu_match.games.iter() {
-            assert_eq!(game.scores.len(), 4);
-        }
-    })
+#[tokio::test]
+async fn get_recent() {
+    let osu_key = init();
+    let osu = Osu::new(osu_key);
+    let _scores = RecentRequest::with_username("mornis")
+        .mode(GameMode::STD)
+        .limit(1)
+        .queue(&osu)
+        .await
+        .unwrap();
+}
+
+#[tokio::test]
+async fn get_match() {
+    let osu_key = init();
+    let osu = Osu::new(osu_key);
+    let osu_match = MatchRequest::with_match_id(58494587)
+        .queue_single(&osu)
+        .await
+        .unwrap();
+    assert_eq!(osu_match.match_id, 58494587);
+    assert_eq!(osu_match.games.len(), 8);
+    for game in osu_match.games.iter() {
+        assert_eq!(game.scores.len(), 4);
+    }
 }
