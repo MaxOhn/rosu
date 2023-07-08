d878cc00e05bbe48ed72dc4bbbe984d6fb53234b
MaxOhn <ohn.m@hotmail.de>
2023-07-08 11:47:22 +0200

    refactor: slight ratelimit refactor

--- src/ratelimit.rs
+++ src/ratelimit.rs
@@ -1,32 +1,43 @@
-use futures_util::lock::Mutex;
 use std::time::{Duration, Instant};
-use tokio::time::sleep;
+use tokio::{sync::Mutex, time::sleep};
 
-/// Basic rate limiter that grants access for a certain amount of times within a time span.
+/// Basic ratelimiter that grants access for a certain amount of times within a time span.
 /// Implemented through token bucket algorithm.
 pub(crate) struct RateLimiter {
     rate: f32,
     rate_per_ms: f32,
-    allowance: Mutex<f32>,
-    last_call: Mutex<Instant>,
+    guarded: Mutex<Inner>,
+}
+
+struct Inner {
+    allowance: f32,
+    last_call: Instant,
 }
 
 impl RateLimiter {
-    /// Creates a new RateLimiter.
+    /// Creates a new [`RateLimiter`].
     /// Allows for up to `rate` amount of access calls within `per_seconds` amount of seconds.
     pub(crate) fn new(rate: u32, per_seconds: u32) -> Self {
         Self {
             rate: rate as f32,
             rate_per_ms: rate as f32 / per_seconds as f32 / 1000.0,
-            allowance: Mutex::new(0.0),
-            last_call: Mutex::new(Instant::now()),
+            guarded: Mutex::new(Inner {
+                allowance: 0.0,
+                last_call: Instant::now(),
+            }),
         }
     }
 
     /// Wait until the next access
     pub(crate) async fn await_access(&self) {
-        let mut allowance = self.allowance.lock().await;
-        let elapsed = self.last_call.lock().await.elapsed().as_millis() as f32; // ms
+        let mut guarded = self.guarded.lock().await;
+
+        let Inner {
+            allowance,
+            last_call,
+        } = &mut *guarded;
+
+        let elapsed = last_call.elapsed().as_millis() as f32; // ms
         *allowance += elapsed * self.rate_per_ms; // msgs
 
         if *allowance > self.rate {
@@ -39,6 +50,6 @@ impl RateLimiter {
             *allowance -= 1.0;
         }
 
-        *self.last_call.lock().await = Instant::now();
+        *last_call = Instant::now();
     }
 }
